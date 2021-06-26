46102e4e62fbed1adf207e538ec7966e5a2c8974
Earther <qn.khuat@gmail.com>
2021-06-26 15:09:40 +0700

    the data get through, but having trouble with the frequency

---
+++ client/src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import { Terminal } from 'xterm';
+//import { FitAddon } from 'xterm-addon-fit';
+import { AttachAddon } from 'xterm-addon-attach';
+import 'xterm/css/xterm.css';
+import base64 from "./base64";
+
+////const websocket = new WebSocket('ws://localhost:8002/s/local/ws');
+const websocket = new WebSocket('ws://0.0.0.0:3000/ws');
+
+function App() {
+
+  useEffect(() => {
+    console.log(websocket)
+    var term = new Terminal({
+      cursorBlink: true,
+      macOptionIsMeta: true,
+      scrollback: 1000,
+      fontSize: 12,
+      letterSpacing: 0,
+      fontFamily: 'SauceCodePro MonoWindows, courier-new, monospace',
+    });
+
+    websocket.onopen = (e: Event) => {
+      console.log("Socket open");
+    }
+
+    websocket.onmessage = (ev: MessageEvent) => {
+      console.log("Got event: ", ev);
+      ev.data.text().then((msg: string) => {
+        let msgObject = JSON.parse(msg)
+        if (msgObject.Type === "Write") {
+          var buffer = base64.base64ToArrayBuffer(msgObject.Data)
+          term.writeUtf8(buffer);
+        }
+      })
+    }
+
+    
+
+
+    //const attachAddon = new AttachAddon(websocket);
+    //term.loadAddon(attachAddon);
+    const terminalDiv = document.getElementById("terminal");
+    if (terminalDiv) {
+      term.open(terminalDiv);
+    }
+
+})
+
+return (
+  <div className="App">
+    <h1>Yooooooooooooooo</h1>
+    <div id="terminal"></div>
+  </div>
+);
+}
+
+export default App;
--- server/main.go
+++ server/main.go
@@ -9,12 +9,20 @@ import (
   "flag"
   "net/http"
   "fmt"
+  "time"
   "os"
 	"os/exec"
   "os/signal"
   "syscall"
   "io"
+  "encoding/json"
 )
+
+type MsgWrapper struct {
+  Type string
+  Data []byte
+}
+
 
 type PtyMaster struct {
   cmd *exec.Cmd
@@ -94,46 +102,72 @@ var httpUpgrader = websocket.Upgrader{
 }
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-  log.Println(fmt.Sprintf("remote addr: %s", r.RemoteAddr))
-  log.Println("YOOOOOOOOOOOOOOOOOOOO")
-  _, err := httpUpgrader.Upgrade(w, r, nil)
+  log.Println("Connecting")
+  httpUpgrader.CheckOrigin = func(r *http.Request) bool { return true }
+  conn, err := httpUpgrader.Upgrade(w, r, nil)
+  defer conn.Close()
   if err != nil {
-    log.Panic("Failed to upgrade to websocket")
+    log.Panicf("Failed to upgrade to websocket: %s", err)
   }
+
+  //go func() {
+    for {
+      buf := make([]byte, 1024)
+      read, err := pty.f.Read(buf)
+      log.Printf("Read byte: %x", buf)
+
+      msg := MsgWrapper{
+        Data: buf[:read],
+        Type: "Write",
+      }
+      
+      payload, err := json.Marshal(msg)
+      if (err != nil) {
+        log.Panicf("Failed to Encode msg: ", err)
+      }
+      if err != nil {
+        conn.WriteMessage(websocket.TextMessage, []byte(err.Error()))
+        log.Panicf("Unable to read from pty/cmd: %s", err)
+        return
+      }
+      conn.WriteMessage(websocket.BinaryMessage, payload)
+      log.Println("Sent a buffer")
+    }
+  //}()
 }
 
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-  log.Printf("checking health")
-  fmt.Fprintf(w, "I'm fine, go away")
+  log.Printf("health check")
+  fmt.Fprintf(w, "I'm fine, go away: %s\n", time.Now().String())
 }
 
+var pty *PtyMaster
 func main() {
   InitLog("log", "")
   var listen = flag.String("listen", "0.0.0.0:3000", "Host:port to listen on")
   log.Println("Start server")
   flag.Parse()
 
-  router := mux.NewRouter()
-  router.HandleFunc("/", healthHandler) // Terminal session
-  //router.HandleFunc("/s", handleWebSocket) // Terminal session
-
-  httpServer := &http.Server{Addr: *listen, Handler:router}
-  go func () {
-    if err := httpServer.ListenAndServe(); err != nil {
-      log.Panicf("Something went wrong with the webserver: %s", err)
-    }
-    log.Println("Http Server is serving at %s", *listen)
-  }()
-
-
-  pty := NewPtyMater()
+  pty = NewPtyMater()
   pty.StartShell()
   log.Println("Shell started")
   // Copy stdin to the pty and the pty to stdout.
   // NOTE: The goroutine will keep reading until the next keystroke before returning.
-  go func() { _, _ = io.Copy(pty.f, os.Stdin) }()
-  _, _ = io.Copy(os.Stdout, pty.f)
+  go func() { io.Copy(pty.f, os.Stdin) }()
+  go func() { io.Copy(os.Stdout, pty.f) }()
+
+  router := mux.NewRouter()
+  router.HandleFunc("/health", healthHandler) // Terminal session
+  router.HandleFunc("/ws", handleWebSocket) // Terminal session
+
+  httpServer := &http.Server{Addr: *listen, Handler:router}
+  go func () {
+    log.Printf("Http Server is serving at %s", *listen)
+    if err := httpServer.ListenAndServe(); err != nil {
+      log.Panicf("Something went wrong with the webserver: %s", err)
+    }
+  }()
 
   pty.Wait()
   pty.Stop()
@@ -141,26 +175,5 @@ func main() {
   fmt.Fprintf(os.Stdout, "Bye!\n")
   log.Println("Stopped")
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
