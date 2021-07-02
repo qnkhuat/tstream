d653a74a1538c7009120e3cc1e8484fb722ce44e
Earther <qn.khuat@gmail.com>
2021-07-02 13:42:09 +0700

    imporove pubsub

--- client/src/lib/pubsub.ts
+++ client/src/lib/pubsub.ts
@@ -1,25 +1,42 @@
+const sep = "_"
 export default class PubSub {
 
   // store callbaccks
   _handlers: {[key: string]: (data: any) => void} = {};
 
-  // strict will allow only one consumer to consume one topic name
+  // set to true if want allow multiple consumers for one topic
   _strict: boolean = false;
 
   constructor(strict: boolean = false) {
     this._strict = strict;
   }
 
-  sub(topic: string, cb: (data: any) => void) {
-    if (!this._handlers[topic]) this._handlers[topic] = cb;
-    else if (!this._strict) this.sub(`topic_${Math.floor(Math.random() * 1000)}`, cb);
-    else throw `${topic} are already subscribed!`;
+  sub(topic: string, cb: (data: any) => void): string | null {
+    if (!this._handlers[topic]) {
+      this._handlers[topic] = cb;
+      return topic
+    } else if (!this._strict) {
+      const topic_name = `topic${sep}${Math.floor(Math.random() * 1000)}`;
+      return this.sub(topic_name, cb);
+    } else { // topic already subscribed and strict is not set
+      return null;
+    }
+  }
+
+  // Unsubscribe a topic
+  // Provide all
+  unsub(topic: string, all: boolean = false) {
+    if (this._handlers[topic]) delete this._handlers[topic] ;
+
+    // search for all hanlders with topic as prefix and delete it
+    if (!this._strict && all) for (const k in this._handlers) if ( k.startsWith(`${topic}${sep}`) ) delete this._handlers[k];
   }
 
   pub(topic: string, msg: any) {
     if(this._strict) {
-      if (topic in this._handlers) this._handlers[topic](msg);
-    } else {
+      if (topic in this._handlers) this._handlers[topic](msg)
+    }
+    else {
       for (const k in this._handlers) if (k.startsWith(k)) this._handlers[k](msg);
     }
   }
