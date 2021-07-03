2c45b25e42cb44a93ef944e0c0eb188335134b30
Earther <qn.khuat@gmail.com>
2021-07-04 04:45:35 +0700

    convert chat to class component

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,10 +1,15 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import * as constants from "../lib/constants";
 import PubSub from "../lib/pubsub";
 import * as base64 from "../lib/base64";
 
 interface Props {
   msgManager: PubSub;
+}
+
+interface State{
+  msgList: ChatMsg[];
+  inputContent: string;
 }
 
 interface ChatMsg {
@@ -18,8 +23,64 @@ interface ChatSec {
   isMe: boolean;
 }
 
+class Chat extends React.Component<Props, State> {
+  state: State = {
+    msgList: [],
+    inputContent: "",
+  }
+
+  componentDidMount() {
+    this.props.msgManager?.sub(constants.MSG_TCHAT, (chatMsg: ChatMsg) => {
+      console.log("msgList in sub is ", this.state.msgList);
+      let newMsgList = this.state.msgList as Array<ChatMsg>;
+      newMsgList.push(chatMsg);
+      this.setState({msgList: newMsgList})
+    })
+
+  }
+
+  onSendMsg(content: string, clearInput: boolean) {
+    let tempMsg = content.trim();
+    if (tempMsg === '') {
+      return;
+    }
+    let data = {
+      Name: "User",
+      Content: tempMsg,
+      Color: "manhcd",
+    };
+    if (clearInput) this.setState({inputContent: ""});
+    this.props.msgManager?.pub(constants.MSG_TREQUEST_CHAT, data);
+  }
+
+  render() {
+    return (
+      <div className="w-full flex flex-col border-l border-gray-500">
+        <div className="h-full bg-black overflow-y-auto overflow-x-none p-2 flex flex-col-reverse">
+          <p>{this.state.msgList.length}</p>
+          {this.state.msgList.map((item, index) => <ChatSection Msg={item} isMe={(index % 2 == 0) ? true : false}/>)}
+        </div>
+        <div className="h-20 border-b border-gray-500 flex-shrink-0 flex items-center justify-between pr-2">
+          <input className="text-white px-3 py-3 flex-grow" placeholder={"Chat with everyone..."} style={{backgroundColor: '#121212'}} value={this.state.inputContent} onChange={(e) => this.setState({inputContent: e.target.value})} />
+          <button className="text-3xl transform hover:scale-125 duration-100" onClick={() => this.onSendMsg('&#128540;', false)}>&#128540;</button>
+        </div>
+        <div className="h-20 flex-shrink-0 flex items-center justify-between px-5 py-3">
+          <div className="flex-grow">
+            <button className="text-red-600 text-4xl transform hover:scale-125 duration-100">&#9829;</button>
+          </div>
+          <button
+            className="px-10 py-2 bg-red-600 text-white rounded flex-shrink-0"
+            onClick = {() => this.onSendMsg(this.state.inputContent, true)}
+          >
+            Send
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
 const ChatSection: React.FC<ChatSec> = ({ Msg, isMe }) => {
-  
   return (
     <>
       <div className={`${isMe ? 'text-right ml-auto mr-0' : ''} w-3/4 flex p-2`}>
@@ -28,74 +89,6 @@ const ChatSection: React.FC<ChatSec> = ({ Msg, isMe }) => {
       </div>
       <div style={{clear: 'both'}}></div>
     </>
-  )
-}
-
-const Chat: React.FC<Props> = ({ msgManager }) => {
-  const [ msgList, setMsgList ] = useState<Array<ChatMsg>>([
-    {
-      Name: "manhcd",
-      Content: "Oi gioi oi tao dep trai nhat trai dat nay aaaaaaaaaa aaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa",
-      Color: "#FA8072",
-    }, 
-    {
-      Name: "trangttt",
-      Content: "Oi gioi oi tao xau trai nhat trai dat nay aaaaaaaaaa aaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa",
-      Color: "#DC143C",
-    },
-  ]);
-  const [ inputContent, setInputContent ] = useState<string>('');
-  var tempList : ChatMsg[] = [];
-
-  console.log("msgList is", msgList);
-  useEffect(() => {
-    msgManager?.sub(constants.MSG_TCHAT, (chatMsg: ChatMsg) => {
-      console.log("msgList in sub is ", msgList);
-      let newMsgList = msgList as Array<ChatMsg>;
-      newMsgList.push(chatMsg);
-      tempList.push(chatMsg);
-      setMsgList(newMsgList);
-    })
-  }, []);
-  
-  function onSendMsg(content: string, clearInput: boolean) {
-    let tempMsg = content.trim();
-    if (tempMsg === '') {
-      return;
-    }     
-    let data = {
-      Name: "User",
-      Content: tempMsg,
-      Color: "manhcd",
-    };     
-    if (clearInput) setInputContent('');
-    msgManager?.pub(constants.MSG_TREQUEST_CHAT, data);
-  }
-
-  console.log("before return: ", msgList.length);
-  console.log("tempList is ", tempList);
-  return (
-    <div className="w-full flex flex-col border-l border-gray-500">
-      <div className="h-full bg-black overflow-y-auto overflow-x-none p-2 flex flex-col-reverse">
-        <p>{msgList.length}</p>
-        {tempList.map((item, index) => <ChatSection Msg={item} isMe={(index % 2 == 0) ? true : false}/>)}
-      </div>
-      <div className="h-20 border-b border-gray-500 flex-shrink-0 flex items-center justify-between pr-2">
-        <input className="text-white px-3 py-3 flex-grow" placeholder={"Chat with everyone..."} style={{backgroundColor: '#121212'}} value={inputContent} onChange={(e) => setInputContent(e.target.value)} /> 
-        <button className="text-3xl transform hover:scale-125 duration-100" onClick={() => onSendMsg('&#128540;', false)}>&#128540;</button>
-      </div>
-      <div className="h-20 flex-shrink-0 flex items-center justify-between px-5 py-3">
-        <div className="flex-grow">
-          <button className="text-red-600 text-4xl transform hover:scale-125 duration-100">&#9829;</button>
-        </div>
-        <button 
-          className="px-10 py-2 bg-red-600 text-white rounded flex-shrink-0"
-          onClick = {() => onSendMsg(inputContent, true)}
-        >
-          Send
-        </button>
-      </div>
-    </div>
   )
 }
 
