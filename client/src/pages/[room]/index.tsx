c196deb1f161e42a39b3db89b932d2c4abf3fdd9
Earther <qn.khuat@gmail.com>
2021-07-05 12:52:00 +0700

    room now is a class component and add display uptime + nviewers

--- client/src/pages/[room]/index.tsx
+++ client/src/pages/[room]/index.tsx
@@ -6,14 +6,17 @@ import * as util from "../../lib/util";
 import * as constants from "../../lib/constants";
 import PubSub from "../../lib/pubsub";
 
-import WSTerminal from "../../components/WSTerminal";
 import Chat from "../../components/Chat";
 import Navbar from "../../components/Navbar";
+import WSTerminal from "../../components/WSTerminal";
+import Uptime from "../../components/Uptime";
 
 import dayjs from "dayjs";
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import PersonIcon from '@material-ui/icons/Person';
 
 const darkTheme = createTheme({
   palette: {
@@ -53,6 +56,7 @@ interface State {
   msgManager: PubSub | null;
   roomInfo: RoomInfo | null;
   ws: WebSocket | null;
+  upTimeStr: string | null;
 }
 
 class Room extends React.Component<Props, State> {
@@ -84,10 +88,12 @@ class Room extends React.Component<Props, State> {
         msgManager.pub(msg.Type, winSizeMsg);
 
       } else if (msg.Type === constants.MSG_TCHAT) {
+
         let encoded_string = "";
         for (let i = 0; i < msg.Data.length; i++) {
           encoded_string = encoded_string.concat(String.fromCharCode(msg.Data[i]));
         }
+
         let chatMsg = JSON.parse(encoded_string);
         msgManager.pub(msg.Type, chatMsg);
       } else if (msg.Type === constants.MSG_ROOM_INFO) {
@@ -95,6 +101,7 @@ class Room extends React.Component<Props, State> {
         let roomInfo = JSON.parse(window.atob(msg.Data));
         this.setState({roomInfo: roomInfo});
         console.log("Got room info: ", roomInfo);
+
 
       }
     }
@@ -123,12 +130,19 @@ class Room extends React.Component<Props, State> {
     })
 
     msgManager.pub("request", constants.MSG_TREQUEST_ROOM_INFO);
+    // TODO: remove this manual request from viewer by actively sending from server
+    // refresh roomInfo every seconds to update number of viewers
+    setInterval(() => {
+      msgManager.pub("request", constants.MSG_TREQUEST_ROOM_INFO);
+    }, 10000);
+
 
     this.state = {
       termSize: null,
       msgManager: msgManager,
       roomInfo: null,
       ws: ws,
+      upTimeStr: null,
     };
 
 
@@ -145,20 +159,27 @@ class Room extends React.Component<Props, State> {
 
   render() {
     return (
-      <div id="room">
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <div
-            ref={this.navbarRef}
-          >
-            <Navbar />
-          </div>
-          {this.state.msgManager && this.state.termSize &&
-          <div id="terminal-view" className="relative flex">
-            <div className="p-1 bg-red-400 rounded absolute top-4 right-4">
-              {this.state.roomInfo &&
-              <p className="text-mdtext-whtie font-semibold">{util.getUpTime(dayjs(this.state.roomInfo.StartedTime))}</p>}
-            </div>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <div ref={this.navbarRef}>
+          <Navbar />
+        </div>
+        {this.state.msgManager && this.state.termSize &&
+        <div id="room" className="flex">
+          <div id="terminal-view" className="relative">
+
+            {this.state.roomInfo &&
+            <>
+              <div id="info-uptime" className="p-1 bg-red-400 rounded absolute top-4 right-4 z-10">
+                <Uptime className="text-md text-white font-semibold" startTime={new Date(this.state.roomInfo.StartedTime)} />
+              </div>
+
+              <div id="info-nviewers" className="p-1 bg-gray-400 rounded absolute bottom-4 right-4 z-10">
+                <p className="text-mdtext-whtie font-semibold"><PersonIcon/> {this.state.roomInfo.NViewers}</p>
+              </div>
+
+            </>
+            }
 
             <WSTerminal
               className="bg-black"
@@ -166,87 +187,16 @@ class Room extends React.Component<Props, State> {
               width={this.state.termSize?.Width ? this.state.termSize.Width : -1}
               height={this.state.termSize?.Height ? this.state.termSize.Height : -1}
             />
-            <Chat
-              msgManager={this.state.msgManager}
-            />
+
           </div>
-          }
-        </ThemeProvider>
-      </div>
+          <Chat
+            msgManager={this.state.msgManager}
+          />
+        </div>
+        }
+      </ThemeProvider>
     )
   }
 }
 
-//function Room() {
-//  const params: Params = useParams();
-//  const chatWinsize = 400; // px
-//
-//  const navbarRef = useRef<HTMLDivElement>(null);
-//  const [ termSize, setTermSize ] = useState<Winsize>();
-//  const [ msgManager, setMsgManager ] = useState<PubSub>();
-//  const [ updateState, setUpdate ] = useState(0);
-//  const [ roomInfo, setRoomInfo ] = useState();
-//  const [ upTime, setUptime ] = useState<string>("");
-//
-//  function resize() {
-//    if (navbarRef.current) {
-//      setTermSize({
-//        Width: window.innerWidth - chatWinsize,
-//        Height: window.innerHeight - navbarRef.current.offsetHeight,
-//      });
-//    }
-//
-//  }
-//
-//  useEffect(() => {
-//    const wsUrl = util.getWsUrl(params.username);
-//    const ws = new WebSocket(wsUrl);
-//
-//    const tempMsg = new PubSub();
-//
-//    ws.onmessage = (ev: MessageEvent) => {
-//      let msg = JSON.parse(ev.data);
-//
-//      if (msg.Type === constants.MSG_TWRITE) {
-//
-//        var buffer = base64.toArrayBuffer(msg.Data)
-//        tempMsg.pub(msg.Type, buffer);
-//
-//      } else if (msg.Type === constants.MSG_TWINSIZE) {
-//
-//        let winSizeMsg = JSON.parse(window.atob(msg.Data));
-//        tempMsg.pub(msg.Type, winSizeMsg);
-//
-//      } else if (msg.Type === constants.MSG_ROOM_INFO) {
-//        let roomInfo = JSON.parse(window.atob(msg.Data));
-//        setRoomInfo(roomInfo);
-//        console.log("Got room info: ", roomInfo);
-//      }
-//    }
-//
-//    tempMsg.sub("request", (msgType: string) => {
-//      var payload_byte = base64.toArrayBuffer(window.btoa(""));
-//      var wrapper = JSON.stringify({
-//        Type: msgType,
-//        Data: Array.from(payload_byte),
-//      });
-//      const payload = base64.toArrayBuffer(window.btoa(wrapper));
-//      util.sendWhenConnected(ws, payload);
-//    })
-//
-//
-//    tempMsg.pub("request", constants.MSG_TREQUEST_ROOM_INFO);
-//
-//
-//    setMsgManager(tempMsg);
-//    window.addEventListener("resize", () => resize());
-//    resize();
-//  }, [])
-//
-//  return (
-
-//  );
-//
-//}
-//
 export default withRouter(Room);
