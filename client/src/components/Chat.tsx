25807fb01325c369ad65eb2466e489f4b8a4359d
manhcd <kooyooman@gmail.com>
2021-07-06 17:48:50 +0700

    init local storage

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -3,6 +3,11 @@ import * as constants from "../lib/constants";
 import PubSub from "../lib/pubsub";
 import * as base64 from "../lib/base64";
 
+interface TstreamUser {
+  name: string,
+  color: string,
+}
+
 interface Props {
   msgManager: PubSub;
 }
@@ -23,15 +28,16 @@ interface State {
   inputContent: string;
   name: string;
   color: string;
+  isWaitingUsername: boolean,
 }
 
 const ChatSection: React.FC<ChatInfo> = ({ Msg, isMe }) => {
 
   return (
     <>
-      <div style={{overflowWrap: "anywhere"}} className={`${isMe ? 'justify-end ml-auto mr-0' : ''} w-3/4 flex p-2`}>
-        {!isMe && <div className="w-28" style={{color: Msg.Color}}>{Msg.Name}: </div>}
-        <div>{Msg.Content}</div>
+      <div className={`${isMe ? 'justify-end ml-auto mr-0' : ''} w-3/4 flex p-2`}>
+        {!isMe && <span style={{color: Msg.Color}}>{Msg.Name}: </span>}
+        <div style={{wordWrap: 'break-word', overflow: 'hidden'}}>{Msg.Content}</div>
       </div>
     </>
   )
@@ -46,9 +52,10 @@ class Chat extends React.Component<Props, State> {
       inputContent: '',
       name: '',
       color: '',
+      isWaitingUsername: false,
     }
   }
-
+  
   addNewMsg(chatInfo: ChatInfo) {
     let newMsgList = this.state.msgList as ChatInfo[];
     newMsgList.push(chatInfo);
@@ -64,11 +71,67 @@ class Chat extends React.Component<Props, State> {
         isMe: false,
       }
       this.addNewMsg(chatInfo);
-    })
+    });
+    
+    const payload = localStorage.getItem('tstreamUser');
+    if (payload !== null) {
+      const tstreamUser : TstreamUser = JSON.parse(payload);
+      this.setState({
+        name: tstreamUser.name,
+        color: tstreamUser.color,
+      });
+    } 
   }
 
   onSendMsg(content: string, clearInput: boolean) {
     let tempMsg = content.trim();
+    if (this.state.name === '' || this.state.color === '') {
+      let notification: string = '';
+      if (!this.state.isWaitingUsername) {
+        notification = "It seems that you're a new user here. Please enter your name in the input section below. Remember your name must not be empty and contain not more than 10 characters."; 
+        
+      }
+      else {
+        if (tempMsg === '' || tempMsg.length > 10) {
+          notification = 'Invalid Username';
+        }
+        else {
+          var color: string = constants.COLOR_LIST[Math.floor(Math.random() * (constants.COLOR_LIST.length))];
+          this.setState({
+            name: tempMsg,
+            color: color,
+          });
+
+          let tstreamUser : TstreamUser = {
+            name: tempMsg,
+            color: color,
+          }
+          const payload = localStorage.setItem('tstreamUser', JSON.stringify(tstreamUser));
+          notification = "Welcome " + tempMsg + " to the TStream !!!!!. Happy a nice day (=^･^=) ...."
+        }
+      }
+
+      let data = {
+        Name: '', 
+        Content: notification,
+        Color: '', 
+      };
+
+      this.setState({
+        inputContent: "",
+      });
+      
+      var chatInfo : ChatInfo = {
+        Msg: data,
+        isMe: true,
+      }
+      this.addNewMsg(chatInfo);
+      this.setState({
+        isWaitingUsername: true,
+      })
+      return ;
+    }
+
     if (tempMsg === '') {
       return;
     }
@@ -94,7 +157,7 @@ class Chat extends React.Component<Props, State> {
     return (
       <div className="w-full flex flex-col border-l border-gray-500 relative" style={{width: "400px"}}>
         <div className="bg-black overflow-y-auto overflow-x-none p-2 flex flex-col-reverse" style={{height: "calc(100vh - 10rem - 57px)"}}>
-          {this.state.msgList.slice(0).reverse().map((item) => <ChatSection Msg={item.Msg} isMe={item.isMe}/>)}
+          {this.state.msgList.slice(0).reverse().map((item, index) => <ChatSection Msg={item.Msg} isMe={item.isMe} key={index}/>)}
         </div>
         <div className="absolute bottom-0 transform w-full">
           <div className="h-20 border-b border-gray-500 flex-shrink-0 flex items-center justify-between pr-2">
@@ -114,24 +177,6 @@ class Chat extends React.Component<Props, State> {
                   this.onSendMsg(this.state.inputContent, true);
                 }
               }}
-              onFocus={() => {
-                if (this.state.name === '' && this.state.color === '') {
-                  var person = prompt("Please enter your name (It must not be empty or have more than 10 characters). If your name is invalid, it is automatically changed to Anonymous.", "");
-                  var color: string = constants.COLOR_LIST[Math.floor(Math.random() * (constants.COLOR_LIST.length))];
-                  var name: string = '';
-
-                  if (person === null || person.length === 0 || person.length > 10) {
-                    name = "Anonymous";
-                  } else {
-                    name = person;
-                  }
-
-                  this.setState({
-                    color: color,
-                    name: name,
-                  });
-                }
-              }}
             />
             {/* <button className="text-3xl transform hover:scale-125 duration-100" onClick={() => this.onSendMsg('&#128540;', false)}>&#128540;</button> */}
           </div>
@@ -141,7 +186,9 @@ class Chat extends React.Component<Props, State> {
             </div> */}
             <button
               className="px-10 py-2 bg-red-600 text-white rounded flex-shrink-0"
-              onClick = {() => this.onSendMsg(this.state.inputContent, true)}
+              onClick = {() => {
+                this.onSendMsg(this.state.inputContent, true)
+              }}
             >
               Send
             </button>
