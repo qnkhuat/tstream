30ca45442f9d38f47a65d4da57f9647696e9d348
Earther <qn.khuat@gmail.com>
2021-07-07 14:54:19 +0700

    sorry but this logo are so much better

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,14 +6,18 @@ interface Props {
 
 const Navbar: React.FC<Props> = () => {
   return (
-    <Link to="/">
-      <div id="navbar" className="flex justify-center py-2 border-b border-black"
-        style={{background:"#18181b"}} >
-        <div className="container" >
-          <img alt={"logo"} className="w-9 h-9" src="./logo2.svg" />
+    <div id="navbar" className="flex justify-center py-2 border-b border-black shadow"
+      style={{background:"#18181b"}} >
+      <div className="container flex justify-between">
+        <Link to="/">
+          <img alt={"logo"} className="w-10 h-10" src="./logo.svg" />
+        </Link>
+        <div className="flex items-center font-bold text-gray-100">
+          <Link to="/how-to" className="border-r border-white pr-4">Stream</Link>
+          <a href="https://github.com/qnkhuat/tstream" className="pl-4">Github</a>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
 
