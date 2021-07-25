4e2b827930b095a82557eeef96b86338869fbccf
Quang Ngoc <qn.khuat@gmail.com>
2021-07-25 18:58:02 +0700

    Delete some files

--- tstream/build.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm --privileged \
-  -v /Users/earther/fun/0_tstream/tstream:/tstream \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /Users/earther/go/src:/go/src \
-  -w /tstream \
-  ghcr.io/gythialy/golang-cross:latest --snapshot --rm-dist
--- tstream/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# exit when any command fails
-set -e
-
-echo "Version: "
-read version
-git tag -a $version -m "Release version $version"
-git push origin $version
-goreleaser release --rm-dist
